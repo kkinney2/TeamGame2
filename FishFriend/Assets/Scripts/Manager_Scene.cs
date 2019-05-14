39ce38b20d00d6216ecb2a396f373d11f2a24931
Unknown <kkinney2@student.fitchburgstate.edu>
2019-05-13 23:34:11 -0400

    MainMenu

---
+++ FishFriend/Assets/Scripts/Manager_Scene.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Manager_Scene : MonoBehaviour {
+
+    public Text ErrorText;
+
+    private void Start()
+    {
+        SetErrorText("");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadNextScene()
+    {
+        if (SceneManager.GetSceneByBuildIndex(GetCurrentSceneIndex() + 1) != null)
+        {
+            LoadByIndex(GetCurrentSceneIndex() + 1); 
+        }
+        else
+        {
+            SetErrorText("Error Loading Level " + GetCurrentSceneIndex() + 1);
+        }
+    }
+
+    public int GetCurrentSceneIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+
+    void SetErrorText(string str)
+    {
+        ErrorText.text = str;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("_MainMenu_");
+    }
+}
--- FishFriend/Assets/Scripts/PickupBehavior.cs
+++ FishFriend/Assets/Scripts/PickupBehavior.cs
@@ -17,7 +17,7 @@ public class PickupBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+
     }
 
     public void ToggleBeingHeld()
